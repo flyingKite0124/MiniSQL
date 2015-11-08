c06f1fad11e20533891439abe48c20b8e6669e45
GreysTone <ars@zju.edu.cn>
2015-11-08 09:22:08 +0800

    Reclean std::string

--- db/db_index.cc
+++ db/db_index.cc
@@ -43,7 +43,8 @@ int CreateCharIndex(Table table, std::string attr_name) {
     // Reset Root Nodes
     BPT_CHAR R;
     R.isLeaf = true;
-//    memset(R.key, 0, sizeof(R.key));
+    for(int i=0; i<CHAR_STRI_ORDER; i++)
+        R.key[i] = "";
     memset(R.pointer, 0, sizeof(R.pointer));
     R.countKey = 0;
     _IndexWriteBlock(index, &R, index.rootAddr);

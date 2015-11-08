c06398c711d77841e6b475003ab2477949a27074
Magica <i@0u0.moe>
2015-11-08 11:47:08 +0800

    Update small test

--- test/smalldata.sql
+++ test/smalldata.sql
@@ -1,6 +1,6 @@
 create table student (
   sno char(8),
-  sname char(16),
+  sname char(16) unique,
   sage int,
   sgender char(1),
   primary key(sno)
@@ -10,4 +10,9 @@ insert into student values ('15406', 'jvarksaes6hlyg', 13, 'F');
 insert into student values ('7028', '5of4tnj6ilj1qk', 26, 'F');
 insert into student values ('11422', '7xgzza0j2l8hyd', 25, 'M');
 insert into student values ('12214', 'ikvxyauy3dvwuk', 7, 'F');
+insert into student values ('23423', 'ikvxyauy3dvwuk', 7, 'F');
+select * from student;
+select * from student where sno = '15056';
+delete from student where sno = '15056';
+select * from student;
 drop table student;
